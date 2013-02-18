f499f141ab2b1cb06d457ca7dbad15a09fa800b3
Gerrit Vos <gevo27@gmail.com>
2013-02-18 16:48:21 +0100

    Twitter module

--- sites/all/themes/hedgecomm/scripts/scripts.js
+++ sites/all/themes/hedgecomm/scripts/scripts.js
@@ -1,7 +1,20 @@
 jQuery(document).ready(function($) {
 
+	// workshop animation
 	$('ul.workshops li').mouseover(function() {
 		$('ul.workshops li .sub').hide();
 		$(this).find('.sub').show();
 	});
+	
+	// slider twitter
+	$('#block-hc-twitter-hc-twitter ul').carouFredSel({
+    items   : 1,
+    direction: "left",
+    scroll : {
+      fx: "fade"
+    },
+    auto : {
+      timeoutDuration: 5000
+    }
+  });
 });
